8bcea2e34f72bb5845a9128c84d8f95c467ca7b4
ZZH <zzh20081023@163.com>
2026-02-20 14:07:03 +0800

    fix:服务器访问的异常

--- src/cuotihunter/urls.py
+++ src/cuotihunter/urls.py
@@ -31,4 +31,7 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # 为了让static文件夹下的所有文件都可以直接访问
+    from django.conf import settings
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
--- src/users/urls.py
+++ src/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('forgot-password/', views.forgot_password_view, name='forgot_password'),
     path('reset-password/<uidb64>/<token>/', views.reset_password_view, name='reset_password'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
+    path('announcement/<int:announcement_id>/', views.announcement_detail_view, name='announcement_detail'),
 ]
